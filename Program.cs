a36f819650d784f5b063e227d9463553d6689428
Marina Melo <engmarinamelo@gmail.com>
2024-05-06 18:52:14 -0300

    Main: Create project

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using bootcamp_avanade.Models;
+
+// Pessoa p = new Pessoa();
+
+// p.Nome = "Marina";
+// p.Idade = 26;
+// p.Apresentar();
+
+// string apresentacao = "Olá, seja bem fudido";
+// int qnt = 90;
+// qnt = 20;
+// double altura = 1.80;
+// decimal preco = 1.80M;
+// bool condicao = true;
+
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da variavel qnt: " + qnt);
+// Console.WriteLine("Valor da variavel altura: " + altura);
+// Console.WriteLine("Valor da variavel preco: " + preco);
+// Console.WriteLine("Valor da variavel condicao: " + condicao);
+
+
+DateTime dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy"));
